459087a7c1fa318c09653646bb09d59aaf2b1591
Saya <s_amangeldinova@kbtu.kz>
2024-02-13 18:50:41 +0600

    3nd lab

---
+++ lab3/2nd task/toDOJava.js
@@ -0,0 +1,35 @@
+const inputBox=document.getElementById("input-box");
+const listPage=document.getElementById("list");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must white something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listPage.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listPage.addEventListener("click", function (e){
+    if(e.target.tagName = "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+}, false);
+
+
+
+function saveData(){
+    localStorage.setItem("data", listPage.innerHTML);
+}
+function showTask(){
+    listPage.innerHTML = localStorage.getItem("data");
+}
+showTask();
