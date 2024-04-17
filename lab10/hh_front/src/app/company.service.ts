3179c1350efa4deff4e249dbc13943fe019579e7
Saya <s_amangeldinova@kbtu.kz>
2024-04-17 21:10:45 +0500

    lab10

---
+++ lab10/hh_front/src/app/company.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {Company} from "./models";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyService {
+  BASE_URL = 'http://127.0.0.1:8000'
+
+  constructor(private http: HttpClient) {}
+
+  getCompanies(): Observable<Company[]> {
+    return this.http.get<Company[]>(
+      `${this.BASE_URL}/api/company/`
+    )
+  }
+
+  createCompany(name: string, description: string, city: string, address: string): Observable<Company> {
+    return this.http.post<Company>(
+      `${this.BASE_URL}/api/company/`,
+      {name, description, city, address}
+    );
+  }
+
+  getVacancies(id: any) {
+    
+  }
+}
